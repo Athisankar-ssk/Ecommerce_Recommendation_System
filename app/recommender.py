02de9c23342e2ec28f4301ba8292906369aed7d9
gitakramsm <akram5960@gmail.com>
2025-09-11 11:21:58 +0530

    removed app

--- app/recommender.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-products = pd.read_csv("data/products.csv")
-
-def get_recommendations(cart_items):
-    categories = products[products['product_id'].isin(cart_items)]['category'].unique()
-    recs = products[products['category'].isin(categories) & ~products['product_id'].isin(cart_items)]
-    return recs.to_dict(orient='records')
